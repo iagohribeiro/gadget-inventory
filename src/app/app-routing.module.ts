0ef936d927c3bb37f720630eb53aaf9cf560c3c7
IagohRibeiro <iagoh.ribeirolima@gmail.com>
2023-10-15 10:43:44 +0200

    Component Creation
    The login screen and registration screen components were created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { FormComponent } from './form/form.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent},
+  {path: 'form', component:FormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

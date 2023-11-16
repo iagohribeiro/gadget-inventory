cc8115bdb6fb8326bb309cc7bdae4be83aa44948
IagohRibeiro <iagoh.ribeirolima@gmail.com>
2023-11-16 22:00:14 +0100

    Form registration and validation
    Validated form fields with Regular Expressions and displayed errors;
    Disabled the submit button while the form was invalid;
    Registered an entity in Web Storage.

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,11 @@
+import { Gadget } from './../gadget';
+import { NgFor } from '@angular/common';
 import { LoginComponent } from './../login/login.component';
-import { Component, ElementRef, ViewChild, Input, Output, EventEmitter, OnInit, OnChanges } from '@angular/core';
+import { Component, ElementRef, ViewChild, Input, Output, EventEmitter, OnInit, OnChanges, NgModule } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import * as M from 'materialize-css';
+import { NgForm } from '@angular/forms';
+import { LocalService } from '../local.service';
 
 @Component({
   selector: 'app-form',
@@ -10,12 +14,20 @@ import * as M from 'materialize-css';
 })
 export class FormComponent implements OnInit, OnChanges {
   @ViewChild('mobile-demo') sideNav?: ElementRef;
+  @ViewChild('myForm') myForm!:NgForm;
 
   @Input() value: string = '';
   @Output() notify = new EventEmitter<string>();
   @Input() isHideMenu:boolean = false;
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  editMode:boolean=false
+  listGadgets:Gadget[]=[];
+  model:string='';
+  brand:string='';
+  serial:string='';
+  coverage:string='';
+
+  constructor(private route: ActivatedRoute, private router: Router, private localStore: LocalService) {}
   
   ngOnChanges():void{
     this.notify.emit('Successful login!');
@@ -28,9 +40,50 @@ export class FormComponent implements OnInit, OnChanges {
         this.isHideMenu=true;
       }
     });
+    Object.keys(localStorage).forEach(data => 
+    {
+      let item = this.localStore.getData(data) || '';
+      if (item != '')
+        this.listGadgets.push(JSON.parse(item) as Gadget)
+    });
   }
 
   ngAfterViewInit(): void{
     M.Sidenav.init(this.sideNav?.nativeElement);
+  }
+
+  submitTemplate(){
+    if(this.editMode)
+    {
+      let indexFind = this.listGadgets.findIndex(x => x.serial == this.serial)
+      this.listGadgets[indexFind].model = this.model;
+      this.listGadgets[indexFind].brand = this.brand;
+      this.listGadgets[indexFind].serial = this.serial;
+      this.listGadgets[indexFind].coverage = this.coverage;
+      this.myForm.reset();
+      this.localStore.saveData(this.listGadgets[indexFind].serial,JSON.stringify(this.listGadgets[indexFind]));
+      this.editMode = false;
+    }
+    else
+    {
+      const newGadget:Gadget = new Gadget(this.model, this.brand, this.serial, this.coverage);
+      this.listGadgets.push(newGadget);
+      console.log(this.listGadgets);
+      this.localStore.saveData(newGadget.serial,JSON.stringify(newGadget));
+      this.myForm.reset();
+    }
+
+  }
+  editGadget(gadget:Gadget){
+    this.model = gadget.model;
+    this.brand = gadget.brand;
+    this.serial = gadget.serial;
+    this.coverage = gadget.coverage;
+    this.editMode = true;
+  }
+  deleteGadget(gadget:Gadget){
+    let indexFind = this.listGadgets.indexOf(gadget);
+    this.listGadgets.splice(indexFind, 1);
+    this.localStore.removeData(gadget.serial);
   }
 }
---
+++ src/app/gadget.ts
@@ -0,0 +1,8 @@
+export class Gadget {
+    constructor(public model:string, public brand:string, public serial:string, public coverage:string){
+        this.model = model;
+        this.brand = brand;
+        this.serial = serial;
+        this.coverage = coverage;
+    }
+}
